ac290fa3443a8c23b1c139c334857de590aeb743
David Whale <david@thinkingbinaries.com>
2017-02-08 16:59:07 +0000

    Can now shake microbit in Z axis and it plays a note with pygame

--- monitor.py
+++ monitor.py
@@ -1,7 +1,16 @@
 import microbit # will auto connect
+import pygame
+
+pygame.mixer.pre_init(22050, -16, 2, 1024)
+pygame.mixer.init()
+
+A = pygame.mixer.Sound("sounds/A.wav")
+
 
 FILENAME = "log.csv"
 f = open(FILENAME, "a+")
+
+playing = False
 
 while True:
     row = microbit.read()
@@ -9,6 +18,12 @@ while True:
         try:
             x,y,z = row.split(' ')
             x,y,z = int(x), int(y), int(z)
+            if not playing and z < -510:
+                A.play()
+                playing = True
+            elif playing and z >= -490:
+                playing = False
+
             print(x,y,z)
             f.write("%d,%d,%d\n" % (x,y,z))
             f.flush()
---
+++ run.sh
@@ -0,0 +1,2 @@
+#! /usr/bin/env bash
+python2.7-32 monitor.py
