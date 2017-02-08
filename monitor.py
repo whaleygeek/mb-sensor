c62a528b31ed2a13f54a3a187141d70a4fffb4d8
David Whale <david@thinkingbinaries.com>
2017-02-08 17:10:04 +0000

    Plays a scale based on x

--- monitor.py
+++ monitor.py
@@ -4,13 +4,16 @@ import pygame
 pygame.mixer.pre_init(22050, -16, 2, 1024)
 pygame.mixer.init()
 
-A = pygame.mixer.Sound("sounds/A.wav")
-
+names = ['C','C#','D','D#','E','F','F#','G','G#','A','A#','B']
+notes = []
+for n in names:
+    notes.append(pygame.mixer.Sound('sounds/%s.wav' % n))
 
 FILENAME = "log.csv"
 f = open(FILENAME, "a+")
 
 playing = False
+index = 0
 
 while True:
     row = microbit.read()
@@ -18,13 +21,24 @@ while True:
         try:
             x,y,z = row.split(' ')
             x,y,z = int(x), int(y), int(z)
-            if not playing and z < -510:
-                A.play()
-                playing = True
-            elif playing and z >= -490:
-                playing = False
 
-            print(x,y,z)
+            x = x + 1024
+            x = min(x, 2048) # 0..2048
+            x = max(x, 0)
+            #print(x)
+
+            x = x / (2048/len(notes))
+            x = min(x, len(notes)-1)
+            notes[x].play()
+            print(x)
+            #if not playing and z < -510:
+            #    notes[index].play()
+            #    playing = True
+            #elif playing and z >= -490:
+            #    playing = False
+            #    index = (index + 1) % len(notes)
+
+            #print(x,y,z)
             f.write("%d,%d,%d\n" % (x,y,z))
             f.flush()
         except ValueError as e:
